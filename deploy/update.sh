6bc729d368a4ac4b50403f7199ecd4526b068b10
muriloarguello <muriloarguello@hotmail.com>
2022-12-19 19:00:49 -0300

    correction of deploy archives

--- deploy/update.sh
+++ deploy/update.sh
@@ -6,6 +6,6 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
-supervisorctl restart profiles_api
+supervisorctl restart api
 
 echo "DONE! :)"
